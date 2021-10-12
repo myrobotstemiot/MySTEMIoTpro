e660c8c7d4552c8a687f2c18c59aa05649e2b299
Thong Toryod <87599310+myrobotstemiot@users.noreply.github.com>
2021-10-12 21:07:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
     website : 'http://www.myrobotstem.com/',
     email : 'myrobotstem@gmail.com',
     git : 'https://github.com/myrobotstemiot/MySTEMIoTpro/',
-    image : '/static/display.jpg',
+    image : '/static/Display.jpg',
     version : '1.0.0',
 };
